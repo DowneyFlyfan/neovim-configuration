13411b421e48fc096a2f75671df2e94d6b9766dd
downeyflyfan <1295222501@qq.com>
2026-02-26 10:21:11 -0800

    Auto-save: Thu Feb 26 10:21:11 PST 2026

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -30,11 +30,20 @@ local ts = require("nvim-treesitter")
 -- install() is a no-op for already-installed parsers
 ts.install(ensure_parsers)
 
--- Enable treesitter highlighting, folding, and indentation via FileType autocmd
-vim.api.nvim_create_autocmd("FileType", {
-	callback = function()
-		pcall(vim.treesitter.start)
+local max_filesize = 150 * 1024
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function(args)
+		local buf = args.buf
+		local filepath = vim.api.nvim_buf_get_name(buf)
+		local ok, stats = pcall(vim.uv.fs_stat, filepath)
+
+		if ok and stats and stats.size > max_filesize then
+			return
+		end
+
+		pcall(vim.treesitter.start, buf)
 		vim.wo[0][0].foldmethod = "expr"
 		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
 		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
